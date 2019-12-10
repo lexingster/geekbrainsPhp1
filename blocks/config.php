6ddfaf75d48509b48a4665b08c85dc9af80a2229
lexingster <44534873+lexingster@users.noreply.github.com>
2019-12-10 14:21:57 +0300

    Create config.php

---
+++ blocks/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$final_width_of_image = 150; //Размер изображения на выходе
+$path_to_image_directory = '../image/'; //Папка загрузки оригиналов
+$path_to_thumbs_directory = '../mimage/';//Папка загрузки миниатюр
+?>
