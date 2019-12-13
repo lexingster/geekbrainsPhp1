a6bc6dedbbb3d0dd41c206404b22cc0e4e7a3b5e
lexingster <44534873+lexingster@users.noreply.github.com>
2019-12-13 14:59:12 +0300

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = "localhost";
+const DB = "gallery";
+const LOGIN = "root";
+const PASS = "";
+
+$connect = mysqli_connect(SERVER,LOGIN,PASS,DB) or 
+    die("Ошибка соединения с базой данных");
