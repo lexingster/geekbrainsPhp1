4e31f17334a053d5a32e7f2524174a7b8b0a5388
lexingster <44534873+lexingster@users.noreply.github.com>
2019-12-23 15:56:39 +0300

    Create config.php

---
+++ core/config.php
@@ -0,0 +1,10 @@
+<?php
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DBNAME", "gallery");
+define("GALLERY_IMAGES_PATH", "img/gallery/");
+define("CATALOG_IMAGES_PATH", "img/catalog/");
+define("DEFAULT_MOD", "catalog");
+define("DEFAULT_ADMIN", "catalog");
+?>
